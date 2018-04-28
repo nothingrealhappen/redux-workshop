ca74ee7f8dc4d19ceb6a6528130cd79a2950c5b7
TW-OY <yjouyang@thoughtworks.com>
2018-04-28 15:04:17 +0800

    add demo example

--- todomvc/src/actions/index.js
+++ todomvc/src/actions/index.js
@@ -1,7 +1,6 @@
 import * as types from '../constants/ActionTypes'
 
-export const addTodo = text => ({ type: types.ADD_TODO, text })
-export const deleteTodo = id => ({ type: types.DELETE_TODO, id })
+// add add& delete action here
 export const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
 export const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
 export const completeAllTodos = () => ({ type: types.COMPLETE_ALL_TODOS })
--- todomvc/src/reducers/todos.js
+++ todomvc/src/reducers/todos.js
@@ -17,21 +17,7 @@ const initialState = [
 
 export default function todos(state = initialState, action) {
   switch (action.type) {
-    case ADD_TODO:
-      return [
-        ...state,
-        {
-          id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-          completed: false,
-          text: action.text
-        }
-      ]
-
-    case DELETE_TODO:
-      return state.filter(todo =>
-        todo.id !== action.id
-      )
-
+    // add ADD & Delete reducer here
     case EDIT_TODO:
       return state.map(todo =>
         todo.id === action.id ?
